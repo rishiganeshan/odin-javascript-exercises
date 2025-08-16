68e730390bd250fcaed3335897db90eb6c5f404d
Rishi Ganeshan <rishi9aneshan@gmail.com>
2025-08-16 14:40:40 +0100

    Add sols for 06-11

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...values) {
+
+
+    for (const value of values) {
+        // console.log(value)
+        let index = arr.lastIndexOf(value);
+        while (index !== -1) {
+            arr.splice(index,1);
+            index = arr.lastIndexOf(value);
+
+        }
+        
+    }
+    return arr
+
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,32 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    if ( typeof startNum != "number" || typeof endNum != "number") {
+        return 'ERROR'
+    }
+        
+    if (startNum < 0 || endNum < 0) {
+
+        return 'ERROR'
+    }
+    
+    if (!Number.isInteger(startNum) || !Number.isInteger(endNum)) {
+
+        return 'ERROR'
+    }
+
+    if (startNum > endNum) {
+        let tmp = endNum
+        endNum = startNum
+        startNum  = tmp
+    }
+
+
+    let arr = []
+
+
+    for (let i = startNum; i <= endNum; i++) {
+        arr.push(i)
+    }
+    return arr.reduce((sum, curr) => sum + curr, 0)
 
 };
 
